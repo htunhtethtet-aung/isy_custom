61e244bb788ae9fd1c9ef54f86eff3a7f72c96f9
htun htet htet-aung <haung@isyedu.org>
2025-01-08 14:07:20 +0630

    fixing field name

--- access_restriction_by_ip/models/res_users.py
+++ access_restriction_by_ip/models/res_users.py
@@ -26,6 +26,6 @@ class ResUsersInherit(models.Model):
     """Inherited res_users for adding new field allowed ip_ids"""
     _inherit = 'res.users'
 
-    allowed_ip_ids = fields.One2many('allowed.ips', 'user_ip_id',
+    allowed_ips = fields.One2many('allowed.ips', 'user_ip_id',
                                      string='IP Address',
                                      help="Allowed ip addresses for the user.")
